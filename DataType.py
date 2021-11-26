86fc1d18f9dd09b302aa3f803df0f5ed6a992590
vaibhavchavan01 <vaibhavchavan495@gmail.com>
2021-11-26 16:59:13 +0530

    26/11/2021 code on datatype operation

---
+++ DataType.py
@@ -0,0 +1,9 @@
+# Assign arbitrary values to the variables such that they are of the types used in the examples
+a ='2'
+b =2
+c =2.1
+d =[1,2,3]
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ NumberOfCharacters.py
@@ -0,0 +1,12 @@
+greeting = "Hello Google!"
+# number of characters stored in the variable greeting
+number_of_char =len(greeting)
+
+# repeat the greetings based on the number of character in 'greeting'
+greetings =greeting*number_of_char
+# number of characters stored in the variable greeting
+number_of_char =len(greeting)
+
+# repeat the greetings based on the number of character in 'greeting'
+greetings =greeting*number_of_char
+print(greetings)
